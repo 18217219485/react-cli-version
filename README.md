6ac4cb140236a7a524c811a230092be64a7f5688
v_chenling <v_chenling@baidu.com>
2018-08-24 11:42:58 +0800

    add codes

---
+++ README.md
@@ -0,0 +1,9 @@
+#react-cli 脚手架
+
+下载npm安装包
+
+npm install react-cli-version -g
+
+初始化项目
+
+react-cli-version init 项目名
---
+++ bin/react-cli.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+const program = require('commander');
+const download = require('download-git-repo');
+const handlebars = require('handlebars');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const chalk = require('chalk');
+const fs = require('fs');
+
+// 交互命令
+const interactionCommand = [
+    {
+        name: 'name',
+        message: 'Project name '
+    },
+    {
+        name: 'author',
+        message: 'Author'
+    },
+    {
+        name: 'description',
+        message: 'Project description (A React.js project)'
+    },
+    {
+        name: 'template',
+        message: 'Choose template(simple-wepack, ant-design)'
+    }
+];
+// 用户交互写入模板
+const handleFileChange = answers => {
+    const fileName = `${answers.name}/package.json`;
+    const meta = {
+        projectName: answers.name,
+        projectAuthor: answers.author,
+        projectDescription: answers.description
+    };
+    if (fs.existsSync(fileName)) {
+        const content = fs.readFileSync(fileName).toString();
+        const result = handlebars.compile(content)(meta);
+        fs.writeFileSync(fileName, result);
+    }
+};
+// 初始化项目
+const handleCreateProject = name => {
+    return inquirer.prompt(interactionCommand).then(answers => {
+        const spinner = ora('正在下载模板');
+        spinner.start();
+        let templateType = answers.template ? answers.template : 'master';
+        download(`github:18217219485/react-templates#${templateType}`, name, {clone: true}, err => {
+            if (err) {
+                spinner.fail();
+                console.log(chalk.red(err));
+            }
+            else {
+                spinner.succeed();
+                handleFileChange(answers);
+                console.log(chalk.green('cd ' + answers.name + '\nnpm install \nnpm run dev'));
+            }
+        });
+    });
+};
+
+program.version('1.0.0', '-v, --version')
+    .command('init <name>')
+    .action(name => {
+        if (!name) {
+            console.log(chalk.red('项目名称必不可少'));
+            return;
+        }
+        else {
+            if (!fs.existsSync(name)) {
+                handleCreateProject(name);
+            }
+            else {
+                console.log(chalk.red('项目已经存在'));
+                return;
+            }
+        }
+    });
+program.parse(process.argv);
